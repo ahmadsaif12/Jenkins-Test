68727a327dd9351902185ed78f348cd7b5276deb
Saif <ahmads87237@gmail.com>
2025-12-05 19:59:27 +0545

    recent changes functions two add two numbers

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+// func adding two numbers
+func Add(a int, b int) int {
+	return a + b
+}
+
 func main() {
+
+	result := Add(8, 4)
+	fmt.Printf(" the additon of two number is %d", result)
 	// print even numbers
 	var n int
 	fmt.Println(" Enter any number :", n)
